e0661e1e17bf42dc695b714956f03d2b85fe29f9
hardline-dev <dklymovets1@gmail.com>
2023-01-06 14:33:59 +0200

    Created teacher model and configured admin panel for teacher

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Teacher
 
-# Register your models here.
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('user', 'scientific_degree', 'academic_status',)
+    list_filter = ('user', 'scientific_degree', 'academic_status',)
+    
+    fieldsets = (
+        (None, {'fields': ('user',)}),
+        ('Teacher\'s information', {'fields': (
+            'scientific_degree', 
+            'academic_status',
+            'office',
+        )}),
+    )
+    
+    search_fields = ('user', 'scientific_degree', 'academic_status',)
+    ordering = ('user',)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from user.models import User
 
-# Create your models here.
+
+class Teacher(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    office = models.CharField(_('Teacher\'s office'), max_length=5, blank=True, null=True)
+
+    class ScientificDegree(models.TextChoices):
+        DOCTOR = 'DS', _('Doctor of Sciences')
+        CANDIDATE = 'CS', _('Candidate of Sciences')
+    
+    scientific_degree = models.CharField(
+        _('Scientific degree'),
+        max_length=2,
+        choices=ScientificDegree.choices
+    )
+
+    class AcademicStatus(models.TextChoices):
+        RESEARCH_OFFICER = 'RO', _('Senior Research Officer')
+        DOCENT = 'DC', _('Docent')
+        PROFESSOR = 'PR', _('Professor')
+
+    academic_status = models.CharField(
+        _('Academic status'),
+        max_length=2,
+        choices=AcademicStatus.choices
+    )
+
+    def __str__(self):
+        return f'{self.user}'
