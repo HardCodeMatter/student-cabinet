5eb7a5e2ebb95ef8039933198b13119a8873d21d
hardline-dev <dklymovets1@gmail.com>
2023-01-15 21:37:51 +0200

    Created register system

--- cabinet/urls.py
+++ cabinet/urls.py
@@ -17,8 +17,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from register import views as view_reg
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls'))
+    path('register/', view_reg.register),
+    path('', include('django.contrib.auth.urls')),
+    path('', include('main.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
