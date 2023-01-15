c6cebab600d1dcb7a9e316e9fe992036b9e4d644
hardline-dev <dklymovets1@gmail.com>
2023-01-15 23:45:45 +0200

    Added layout and basic logic for user's profile view and creating.

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('profile/', views.profile_view, name='profile_view'),
+    path('profile/create/', views.profile_create, name='profile_create'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
+from user.models import User
+from .models import Teacher, Student
 
 
 def index(request):
     return render(request, 'main/base.html', {})
+
+def profile_view(request):
+    user = User.objects.get(id=request.user.id)
+    profile = None
+
+    try:
+        if user.type[0]:
+            profile = Student.objects.get(user=user.id)
+        elif user.type[1]:
+            profile = Teacher.objects.get(user=user.id)
+    except ObjectDoesNotExist:
+        profile = None
+
+
+    return render(request, 'main/profile-view.html', {'profile': profile})
+
+def profile_create(request):
+    return render(request, 'main/profile-create.html', {})
